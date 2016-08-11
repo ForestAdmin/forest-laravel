f58687722ce60179f4c15fe993df7812e4e7f685
dib258 <lesmanne@gmail.com>
2016-08-11 17:50:44 +0200

    Added space for better visibility

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -4,7 +4,7 @@ use Illuminate\Routing\Router;
 
 Route::group(['middleware' => 'cors'], function(Router $router) {
     $spacename = "\ForestAdmin\ForestLaravel\Http\Controllers";
-    
+
     Route::get('forest', $spacename.'\ForestController@index');
     Route::post('forest/sessions', $spacename.'\ForestController@sessions');
 
