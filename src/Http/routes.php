6e854674f0a01be4c649d811c09d17ba0b7eab5e
dib258 <lesmanne@gmail.com>
2016-08-23 17:40:44 +0200

    Added new route

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -8,8 +8,11 @@ Route::group(['middleware' => 'cors'], function(Router $router) {
     Route::get('forest', $spacename.'\ForestController@index');
     Route::post('forest/sessions', $spacename.'\ForestController@sessions');
 
-    Route::get('forest/post', $spacename.'ForestController@post');
+    Route::get('forest/post', $spacename.'\ForestController@post');
 
-    Route::get('forest/{modelName}/{recordId}', $spacename.'LianaController@getResource');
+    Route::get('forest/{modelName}/{recordId}', $spacename.'\LianaController@getResource');
+    Route::get('forest/{modelName}', $spacename.'\LianaController@listResources');
+    Route::post('forest/{modelName}', $spacename.'\LianaController@createResource');
+    Route::put('forest/{modelName}/{recordId}', $spacename.'\LianaController@updateResource');
 });
 
