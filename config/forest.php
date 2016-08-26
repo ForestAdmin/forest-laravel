60e980cb659abea13e08b0a9b76f0272c9b98ae5
dib258 <lesmanne@gmail.com>
2016-08-26 16:20:05 +0200

    Cleaned forest config file

--- config/forest.php
+++ config/forest.php
@@ -8,11 +8,11 @@ return [
      |
      |
      */
-//    'URI' => 'http://app.forestadmin.com',
+
     'ApiMap' => 'https://forestadmin-server.herokuapp.com/forest/apimaps',
     'URI' => 'https://forestadmin-server.herokuapp.com',
     'SecretKey' => '4d737c706cbd622b45820ecfe6669cab36e26ea600df8e35a69b91502cb9749f',
-    'AuthKey' => 'FGASGSGDDSG',
+    'AuthKey' => env('APP_KEY'),
 
     /*
     |--------------------------------------------------------------------------
