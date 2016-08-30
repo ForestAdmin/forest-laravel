732676ee48fbb6ae56d953824c3e494adb99e857
dib258 <lesmanne@gmail.com>
2016-08-30 10:18:29 +0200

    Changed to standart string

--- config/forest.php
+++ config/forest.php
@@ -11,8 +11,8 @@ return [
 
     'ApiMap' => 'https://forestadmin-server.herokuapp.com/forest/apimaps',
     'URI' => 'https://forestadmin-server.herokuapp.com',
-    'SecretKey' => '4d737c706cbd622b45820ecfe6669cab36e26ea600df8e35a69b91502cb9749f',
-    'AuthKey' => env('APP_KEY'),
+    'SecretKey' => 'SecretKeyGeneratedFromForestAdminWebsite',
+    'AuthKey' => 'YourSecretKey',
 
     /*
     |--------------------------------------------------------------------------
