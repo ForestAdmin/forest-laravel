1948be4df4b1ac2c2862a225ce8ba621e0f574ba
Arnaud Besnier <arnaudibesnier@gmail.com>
2017-02-07 18:54:17 +0100

    Smart Action - Smart actions support (#29)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,8 @@
 # Change Log
 
 ## [Unreleased]
+### Added
+- Smart Action - Smart actions support.
 
 ## RELEASE 0.0.15 - 2016-02-07
 ### Fixed
---
+++ src/Schema/ActionSchema.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace ForestAdmin\ForestLaravel\Schema;
+
+use Neomerx\JsonApi\Schema\SchemaProvider;
+
+class ActionSchema extends SchemaProvider {
+    protected $resourceType = 'actions';
+
+    public function getId($action) {
+        return $action->getCollection()->getName().'.'.$action->getName();
+    }
+
+    public function getAttributes($action) {
+        $attributes = array();
+        $attributes['name'] = $action->getName();
+        $attributes['endpoint'] = $action->getEndpoint();
+        $attributes['http-method'] = $action->getHttpMethod();
+        $attributes['redirect'] = $action->getRedirect();
+        $attributes['download'] = $action->getDownload();
+
+        return $attributes;
+    }
+}
